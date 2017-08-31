d011502b17532a0c15354201d863faa4de445d5f
aom-yambas <aom-yambas@ip-172-31-8-2.eu-central-1.compute.internal>
2017-08-31 10:52:44 +0000

    [AOM] Change MetaModel

--- src/com/apiomat/nativemodule/gittest123/TestResourceAccess.java
+++ src/com/apiomat/nativemodule/gittest123/TestResourceAccess.java
@@ -63,6 +63,8 @@ public class TestResourceAccess extends AbstractClientDataModel implements IMode
     /** class specific attributes */
     @StaticData( type = com.apiomat.nativemodule.StaticData.Type.File )
     private String fileURL;
+    @StaticData( type = com.apiomat.nativemodule.StaticData.Type.Image )
+    private String imageURL;
     /**
      * Protected constructor; to create a new instance, use the createObject() method
      */
@@ -121,6 +123,70 @@ public class TestResourceAccess extends AbstractClientDataModel implements IMode
     {
         String url = saveResource( data, false, fileName, format );
         setFileURL( url );
+        return url;
+    }
+
+    public String getImageURL( )
+    {
+        String url;
+        url = this.imageURL;
+        return url;
+    }
+
+    public byte[] loadImage( )
+    {
+        String resUrl = getImageURL()+".img";
+        return loadResource(resUrl);
+    }
+
+    public String getImageURL( String apiKey, String system, int width, int height, 
+        String backgroundColorAsHex, Double alpha, String format )
+    {
+        final StringBuilder additionalParameters = new StringBuilder();
+        additionalParameters.append( ".img?apiKey=" );
+        additionalParameters.append( apiKey );
+        additionalParameters.append( "&system=" );
+        additionalParameters.append( system );
+        additionalParameters.append( "&width=" );
+        additionalParameters.append( width );
+        additionalParameters.append( "&height=" );
+        additionalParameters.append( height );
+        
+        if(backgroundColorAsHex != null) 
+        {
+            additionalParameters.append( "&bgcolor=" );
+            additionalParameters.append( backgroundColorAsHex );
+        }
+        if(alpha != null)
+        {
+            additionalParameters.append( "&alpha=" );
+            additionalParameters.append( alpha );
+        }
+        if(format != null)
+        {
+            additionalParameters.append( "&format=" );
+            additionalParameters.append( format );
+        }
+        return getImageURL( ) + additionalParameters;
+    }
+
+    public byte[] loadImage( String apiKey, String system, int width, int height, 
+        String backgroundColorAsHex, Double alpha, String format )
+    {
+        final String resUrl = getImageURL( apiKey, system, width, height, 
+            backgroundColorAsHex, alpha, format );
+        return loadResource(resUrl);
+    }
+
+    public void setImageURL( String url ) 
+    {
+        this.imageURL = url;
+    }
+
+    public String postImage( byte[] data , String fileName, String format )
+    {
+        String url = saveResource( data, true, fileName, format );
+        setImageURL( url );
         return url;
     }
 
